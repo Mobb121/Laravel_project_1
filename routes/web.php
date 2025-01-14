f0ff05fe541e61f785580c5c730d28c254af0fc4
Mobb121 <mobb121@mail.ru>
2025-01-14 21:27:47 +0300

    установил авторизацию, подключил ботстрап, сделал таблицу в sqlite

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
